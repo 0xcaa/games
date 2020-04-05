03bd3d2dcb228cb961f571856016e83b649cdc85
bixvap <ctrigger@protonmail.com>
2020-04-05 21:22:43 +0200

    working on tictactoe

--- tictactoe.c
+++ tictactoe.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 #define SIZE 3
+#define SIZE_B 4
 
 int printboard(char board[SIZE][SIZE]);
+int getinput(int *i, int *j);
+int usage(void);
 
 int main(void)
 {
@@ -10,12 +14,50 @@ int main(void)
                                {'_','_','_'},
                                {'_','_','_'} };
 
+    int i, j;
     char *bp = &board[0][0];
 
-    board[0][0] = 'o';
-    board[1][0] = 'x';
-    
-    printboard(board); 
+    //board[0][0] = 'o';
+    //board[1][0] = 'x';
+
+    usage(); 
+    getinput(&i, &j);
+    printf("%d %d", i, j);
+
+    return 0;
+}
+
+int getinput(int *i, int *j)
+{
+    //int i, j;
+    printf("type: ");
+    scanf("%d", i);
+    scanf("%d", j);
+
+
+    if(*i<0||*i>2)
+        usage();
+    if(*j<0||*j>2)
+        usage();
+
+    return 0;
+}
+
+int usage(void)
+{
+    int i, j;
+
+    printf("first number controls the x axis\nsecound number control the y axis\n");
+    char board[SIZE_B][SIZE_B] = { {'x','0','1','2'},
+                                   {'0','_','_','_'},
+                                   {'1','_','_','_'},
+                                   {'2','_','_','_'} };
+    for(i=0;i<SIZE_B;i++){
+        for(j=0;j<SIZE_B;j++)
+            printf("%c  ", *(*(board + i) + j));
+
+        printf("\n");
+    }
 
     return 0;
 }
